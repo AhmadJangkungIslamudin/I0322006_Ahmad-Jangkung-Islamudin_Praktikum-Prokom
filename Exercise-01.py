3c3073aff36ffdc3987a86199ca5aae50ccbb46b
AhmadJangkungIslamudin <ahmadjangkungi@student.uns.ac.id>
2023-03-13 16:00:01 +0700

    Exercise-01

---
+++ Exercise-01.py
@@ -0,0 +1,15 @@
+print("""
+Nama = Ahmad Jangkung Islamudin
+NIM  = I0322006
+
+Saya berasal Kota Purwodadi, Kabupaten Grobogan. Tempat tinggal saya tepatnya di Desa Karanganyar.
+Desa saya termasuk ramai karena menjadi jalan utama dan bisa juga sebagai jalur alternatif. 
+Kebetulan rumah saya itu sebagai toko dan juga berada di pinggir jalan utama, menjadi keuntungan tersendiri.
+Lingkungan di sekitar rumah saya banyak dipakai untuk berjualan karena tempatnya yang strategis. 
+Di depan rumah saya terdapat perkarangan, pos ronda, dan juga sungai. 
+Pada saat banjir besar, air sungai di depan rumah saya bisa meluap sampai perkarangan tersebut tenggelam dan apabila 
+dilihat dari rumah saya seperti terdapat laut didepan rumah saya.
+
+Harapan saya menjadi lulusan Teknik Industri yaitu mampu mendapatkan ilmu-ilmu yang bermanfaat sebagai bekal ke depannya 
+tidak hanya di dunia pekerjaan saja, akan tetapi berguna di kehidupan sehari-hari.
+""")
